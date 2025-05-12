b8bf9a465c0f2dcf26bf67fb0dbb9d97b636ef72
Md. Niaz Mahmud Shihab <niazmahmud.shihab@gmail.com>
2025-05-13 05:05:53 +0600

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -9,9 +9,9 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = var.az_count
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
