053ebf0159419aa000a4e4da3ea8a93d29138c00
n1azmahmud <niazmahmud.shihab@gmail.com>
2025-05-13 05:01:06 +0600

    Added main.tf script in modules>vpc

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.cluster_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.cluster_name}-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
+}
